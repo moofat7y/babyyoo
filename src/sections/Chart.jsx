82dbc3f7130983ce5bd54dbbc7d8cb6b02030b40
moofat7y <mohammedbelal802@gmail.com>
2023-10-16 02:47:18 +0300

    thrid commit

--- src/sections/Chart.jsx
+++ src/sections/Chart.jsx
@@ -2,17 +2,28 @@ import React from "react";
 import Section from "../components/Section";
 import MainHeader from "../components/ui/MainHeader";
 import chart from "../assets/03.png";
+import logo from "../assets/Yooshicoin_02.png";
 
 export default function Chart() {
   return (
     <Section id={"charts"}>
       <div className="container">
         <MainHeader title={"CHART"} header={"Tokenomics"} />
-        <div className="flex justify-center">
+        <div className="flex flex-col justify-center items-center">
           <img
             src={chart}
             className="object-contain w-[90%] md:w-[30%] min-w-[310px]"
           />
+
+          <div className="flex items-center mt-8 justify-center gap-3">
+            <img src={logo} className="w-[60px] h-[60px] object-contain" />
+            <h4
+              style={{ fontFamily: "sans-serif" }}
+              className="text-center font-black text-3xl"
+            >
+              Total : 1.000.000.000
+            </h4>
+          </div>
         </div>
       </div>
     </Section>
